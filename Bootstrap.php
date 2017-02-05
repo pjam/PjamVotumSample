eaa0d98530cac8d1f572b20ad4e047cd7ff968f5
Flavio Rosa <flaviorosa@gmail.com>
2017-02-05 16:08:15 +0000

    added Bootstrap file with methods for plugin information and adding/removing required field from database on plugin installation/uninstallation

---
+++ Bootstrap.php
@@ -0,0 +1,61 @@
+<?php
+
+class Shopware_Plugins_Frontend_PjamVotumSample_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+{
+    // returns plugin's current version
+    public function getVersion()
+    {
+        return '0.1.0';
+    }
+
+    // returns plugin's label
+    public function getLabel()
+    {
+        return 'Votum Sample Plugin';
+    }
+
+    // gets plugin's information
+    public function getInfo()
+    {
+        return array(
+            'label' => $this->getLabel(),
+            'version' => $this->getVersion(),
+            'link' => 'https://github.com/pjam/pjamVotumSample'
+        );
+    }
+
+    // method called on plugin installation
+    public function install()
+    {
+        // get the crud service, which is responsible for managing the database
+        $service = $this->get('shopware_attribute.crud_service');
+        // inserts (if it does not exists) the field "votum_customer_number" with type "string"
+        // into table "s_user_attributes"
+        $service->update('s_user_attributes', 'votum_customer_number', 'string');
+
+        // rebuild attribute models (recommended
+        // https://developers.shopware.com/developers-guide/attribute-system/#rebuild-attribute-models)
+        $metaDataCache = Shopware()->Models()->getConfiguration()->getMetadataCacheImpl();
+        $metaDataCache->deleteAll();
+        Shopware()->Models()->generateAttributeModels(['s_user_attributes']);
+
+        return true;
+    }
+
+    // method called on plugin uninstallation
+    public function uninstall()
+    {
+        // get the crud service, which is responsible for managing the database
+        $service = $this->get('shopware_attribute.crud_service');
+        // deletes the field "votum_customer_number" from table "s_user_attributes"
+        $service->delete('s_user_attributes', 'votum_customer_number');
+
+        // rebuild attribute models (recommended
+        // https://developers.shopware.com/developers-guide/attribute-system/#rebuild-attribute-models)
+        $metaDataCache = Shopware()->Models()->getConfiguration()->getMetadataCacheImpl();
+        $metaDataCache->deleteAll();
+        Shopware()->Models()->generateAttributeModels(['s_user_attributes']);
+
+        return true;
+    }
+}
